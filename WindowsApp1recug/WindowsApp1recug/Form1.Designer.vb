af8ed89d4e33cc63b893ef6ac5251a2f196d421d
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-06-11 12:03:09 -0400

    Add files via upload

---
+++ WindowsApp1recug/WindowsApp1recug/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.PRACTICOToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EJERCICIO1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EJERCICIO2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(245, 141)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 120
+        Me.Button4.Text = "Limpiar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(134, 141)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 119
+        Me.Button3.Text = "Marco"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(67, 382)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 118
+        Me.Label2.Text = "Label2"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(540, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 117
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(134, 183)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(445, 221)
+        Me.PictureBox1.TabIndex = 116
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(506, 81)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox5.TabIndex = 115
+        Me.TextBox5.Text = "4"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(410, 81)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox4.TabIndex = 114
+        Me.TextBox4.Text = "180"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(310, 81)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox3.TabIndex = 113
+        Me.TextBox3.Text = "20"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(231, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox2.TabIndex = 112
+        Me.TextBox2.Text = "350"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(134, 81)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(73, 20)
+        Me.TextBox1.TabIndex = 111
+        Me.TextBox1.Text = "20"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PRACTICOToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(724, 24)
+        Me.MenuStrip1.TabIndex = 121
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'PRACTICOToolStripMenuItem
+        '
+        Me.PRACTICOToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EJERCICIO1ToolStripMenuItem, Me.EJERCICIO2ToolStripMenuItem})
+        Me.PRACTICOToolStripMenuItem.Name = "PRACTICOToolStripMenuItem"
+        Me.PRACTICOToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.PRACTICOToolStripMenuItem.Text = "MENU"
+        '
+        'EJERCICIO1ToolStripMenuItem
+        '
+        Me.EJERCICIO1ToolStripMenuItem.Name = "EJERCICIO1ToolStripMenuItem"
+        Me.EJERCICIO1ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EJERCICIO1ToolStripMenuItem.Text = "EJERCICIO 1"
+        '
+        'EJERCICIO2ToolStripMenuItem
+        '
+        Me.EJERCICIO2ToolStripMenuItem.Name = "EJERCICIO2ToolStripMenuItem"
+        Me.EJERCICIO2ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.EJERCICIO2ToolStripMenuItem.Text = "EJERCICIO 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(724, 480)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents PRACTICOToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EJERCICIO1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EJERCICIO2ToolStripMenuItem As ToolStripMenuItem
+End Class
